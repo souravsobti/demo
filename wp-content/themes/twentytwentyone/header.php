c0ed00c6fbde4b5d184ba2491ff37a64b2c61ef3
souravsobti <souravsobti@gmail.com>
2021-04-21 16:48:18 +0530

    upload single

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -23,11 +23,12 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<h1> Local Change 1111112222</h1>   
+	<h1> Local Change</h1>   
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
 
 	<?php get_template_part( 'template-parts/header/site-header' ); ?>
 
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+			<main id="main" class="site-main" role="main">  
+			p
